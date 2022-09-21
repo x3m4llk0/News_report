13d32eb64cb9c95ee67f179657585f0429374ce9
Егор Мухаметвафин <egor190420000@gmail.com>
2022-09-21 20:51:52 +0300

    Добавлено эхо

--- tgbot/handlers/echo.py
+++ tgbot/handlers/echo.py
@@ -7,12 +7,8 @@ echo_router = Router()
 @echo_router.message(F.text, state=None)
 async def bot_echo(message: types.Message):
     text = [
-        [
-            f"Команды {hbold(message.text)} нету.\n",
-            f"Для просмотра команд выполните команду /menu либо /start",
-        ]
+        f"Команды {hbold(message.text)} нету.\n",
+        f"Для просмотра команд выполните команду /menu либо /start",
     ]
 
     await message.answer('\n'.join(text))
-
-
